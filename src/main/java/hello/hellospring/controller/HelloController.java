d9d67dbfd973df5fa8bc1538071c9463bf557473
박관우 <pgw4712@icloud.com>
2022-01-16 03:06:24 +0900

    create Repository

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+
+        model.addAttribute("name", "박관우");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // http header 와 body중 body부분에 내용을 직접 넣어 주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        // view가 존재하지 않음
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // Json 방식으로 return
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    // 필드주입 : 중간에 바꿀수 없는 불편함이 있다.
+    // @Autowired private MemberService memberService;
+
+    // Setter 주입 : Controller에서 접근하기 위해서 public으로 설정해야하지만 public하게 노출의 위험이 있다.
+    // @Autowired
+    // public void setMemberService(MemberService memberService) {
+    //   this.memberService = memberService;
+    // }
+
+    private final MemberService memberService;
+
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
+}
